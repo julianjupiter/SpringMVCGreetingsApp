b6b1afdd2e3a69d9fcb0683e3928aa0f75ceb241
julianjupiter <julianjupiter.io@gmail.com>
2020-07-26 19:25:18 +0800

    Remove unused code and import.

--- src/main/java/com/julianjupiter/springmvcgreetings/server/TomcatServer.java
+++ src/main/java/com/julianjupiter/springmvcgreetings/server/TomcatServer.java
@@ -4,7 +4,6 @@ import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.WebResourceRoot;
 import org.apache.catalina.startup.Tomcat;
-import org.apache.catalina.webresources.DirResourceSet;
 import org.apache.catalina.webresources.StandardRoot;
 
 import java.io.File;
